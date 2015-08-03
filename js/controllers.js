fe2dcd1850f66e1affa700df5ad268469cefe41f
Robert Henderson <rhenderson@vlocity.com>
2015-08-03 11:46:54 -0700

    Cleaned up and integrated Supreeth's JS http://plnkr.co/edit/N5hpyDKbO1A7tUv03KiV?p=preview

--- js/controllers.js
+++ js/controllers.js
@@ -2,10 +2,14 @@
 
 /* Controllers */
 
-var policyCards = angular.module("hpCompare", ['ui.bootstrap','angular.filter']);
+var compare = angular.module("compare", ['ui.bootstrap']);
 
-policyCards.controller("hpCompareController", function($scope) {
-    $scope.products = {
+compare.controller("compareController", function($scope) {
+    $scope.name = 'Supreeth';
+    $scope.categoryList = [];
+    $scope.categoryMap = {};
+    $scope.productList = [];
+    $scope.productsJSON = {
         "TFDRresp": {
             "output": [{
                 "attributes": [{
@@ -32,16 +36,16 @@ policyCards.controller("hpCompareController", function($scope) {
                     "categoryName": "Preventive Care",
                     "value": "45",
                     "name": "Another Name"
-                }, ],
-                "NumDays": 1,
+                }],
+                "NumDays": "1",
                 "ProductName": "HMO 200",
-                "ProductRecommend": true,
+                "ProductRecommend": "true",
                 "ProductCode": "TI-AI",
-                "NumChildren": 2,
-                "NumAdults": 2,
-                "TotalPrice": 26.8,
-                "ChildPrice": 3.4,
-                "AdultPrice": 7.5,
+                "NumChildren": "2",
+                "NumAdults": "2",
+                "TotalPrice": "26.8",
+                "ChildPrice": "3.4",
+                "AdultPrice": "7.5",
                 "ID": "0"
             }, {
                 "attributes": [{
@@ -68,16 +72,16 @@ policyCards.controller("hpCompareController", function($scope) {
                     "categoryName": "Preventive Care",
                     "value": "45",
                     "name": "Non-Physican Office Visit"
-                }, ],
-                "NumDays": 1,
+                }],
+                "NumDays": "1",
                 "ProductName": "HMO 250",
-                "ProductRecommend": false,
+                "ProductRecommend": "false",
                 "ProductCode": "TI-EM",
-                "NumChildren": 2,
-                "NumAdults": 2,
-                "TotalPrice": 12.9,
-                "ChildPrice": 1.2,
-                "AdultPrice": 2.75,
+                "NumChildren": "2",
+                "NumAdults": "2",
+                "TotalPrice": "12.9",
+                "ChildPrice": "1.2",
+                "AdultPrice": "2.75",
                 "ID": "1"
             }, {
                 "attributes": [{
@@ -100,19 +104,77 @@ policyCards.controller("hpCompareController", function($scope) {
                     "categoryName": "Preventive Care",
                     "value": "45",
                     "name": "Non-Physican Office Visit"
-                }, ],
-                "NumDays": 1,
+                }],
+                "NumDays": "1",
                 "ProductName": "HMO 500",
-                "ProductRecommend": false,
+                "ProductRecommend": "false",
                 "ProductCode": "TI-NE",
-                "NumChildren": 2,
-                "NumAdults": 2,
-                "TotalPrice": 10.1,
-                "ChildPrice": 0.8,
-                "AdultPrice": 1.75,
+                "NumChildren": "2",
+                "NumAdults": "2",
+                "TotalPrice": "10.1",
+                "ChildPrice": "0.8",
+                "AdultPrice": "1.75",
                 "ID": "2"
-            }, ]
+            }]
         },
         "error": "OK"
+    };
+
+    $scope.attributesValueForProduct = function(productCode, categoryCode, attributeName) {
+        return $scope.categoryMap[categoryCode]['attributes'][attributeName]['assignmentValues'][productCode];
     }
+
+    $scope.transformData = function() {
+        var products = $scope.productsJSON['TFDRresp']['output'];
+        for (var i = 0; i < products.length; i++) {
+            var productAttributes = products[i];
+            var product = {};
+            product['name'] = productAttributes['ProductName'];
+            product['code'] = productAttributes['ProductCode'];
+
+            $scope.productList.push(product);
+            var attributes = productAttributes['attributes'];
+            for (var j = 0; j < attributes.length; j++) {
+                var attributeAssignment = attributes[j];
+                var categoryName = attributeAssignment['categoryName'];
+                var attributeName = attributeAssignment['name'];
+                var attributeValue = attributeAssignment['value'];
+
+                if ($scope.categoryMap[categoryName] == null) {
+                    $scope.categoryList.push(categoryName);
+                    var attributeMap = {};
+                    var productAssignmentMap = {};
+                    productAssignmentMap[product['code']] = attributeValue;
+                    attributeMap['assignmentValues'] = productAssignmentMap;
+                    attributeMap['name'] = attributeName;
+                    $scope.categoryMap[categoryName] = {};
+                    $scope.categoryMap[categoryName]['attributes'] = {};
+                    $scope.categoryMap[categoryName]['attributes'][attributeName] = attributeMap;
+                } else {
+                    var attributeMap = $scope.categoryMap[categoryName]['attributes'][attributeName];
+                    if (attributeMap != null) {
+                        var productAssignmentMap = attributeMap['assignmentValues'];
+                        productAssignmentMap[product['code']] = attributeValue;
+                    } else {
+                        attributeMap = {};
+                        var productAssignmentMap = {};
+                        productAssignmentMap[product['code']] = attributeValue;
+                        attributeMap['assignmentValues'] = productAssignmentMap;
+                        attributeMap['name'] = attributeName;
+                        $scope.categoryMap[categoryName]['attributes'][attributeName] = attributeMap;
+                    }
+                }
+            }
+        }
+    }
+
+    $scope.showCategory = function(category) {
+        return ($scope.filteredCategory == null || category == $scope.filteredCategory);
+    }
+
+    $scope.selectedItemChanged = function(code) {
+        $scope.filteredCategory = code;
+    }
+
+    $scope.transformData();
 });
