002abbec3fef49e88816cf253862f7d78541e1d4
Robert Henderson <rhenderson@vlocity.com>
2015-08-07 10:36:00 -0700

    Added in selectable items from omniscript and integrated them in with products dataset.

--- js/controllers.js
+++ js/controllers.js
@@ -5,160 +5,203 @@
 var bpModule = angular.module("compare", ['ui.bootstrap']);
 
 bpModule.controller("compareController", function($scope, $modal, $log) {
-    $scope.productsJSON = {
-        "recSet": [{
-            "attributes": [{
-                "categoryName": "Deductible",
-                "value": "4000",
-                "dataType": "Currency",
-                "name": "Annual Deductible"
+    $scope.control = {
+        "vlcSI": {
+            "recSet": [{
+                "attributes": [{
+                    "categoryName": "Deductible",
+                    "value": "4000",
+                    "dataType": "Currency",
+                    "name": "Annual Deductible",
+                    "description": "Key"
+                }, {
+                    "categoryName": "Deductible",
+                    "value": "medical deductible",
+                    "dataType": "Text",
+                    "name": "Prescription Drug Deductible",
+                    "description": "Key"
+                }, {
+                    "categoryName": "Preventive Care",
+                    "value": "30",
+                    "dataType": "Percent",
+                    "name": "Primary Care Office Visit",
+                    "description": "Key"
+                }, {
+                    "categoryName": "Preventive Care",
+                    "value": "40",
+                    "dataType": "Currency",
+                    "name": "Speciality Office Visit",
+                    "description": "Key"
+                }, {
+                    "categoryName": "Preventive Care",
+                    "value": "45",
+                    "dataType": "Currency",
+                    "name": "Non-Physican Office Visit",
+                    "description": "Key"
+                }, {
+                    "categoryName": "Preventive Care",
+                    "value": "45",
+                    "dataType": "Currency",
+                    "name": "Another Name"
+                }],
+                "NumDays": "1",
+                "ProductName": "Tiered Copayment HMO 200",
+                "ProductRecommend": false,
+                "ProductCode": "TI-AI",
+                "NumChildren": "2",
+                "NumAdults": "2",
+                "TotalPrice": "26.8",
+                "ChildPrice": "3.4",
+                "AdultPrice": "7.5",
+                "ID": "0",
+                "vlcSelected": true
             }, {
-                "categoryName": "Deductible",
-                "value": "medical deductible",
-                "dataType": "Text",
-                "name": "Prescription Drug Deductible"
+                "attributes": [{
+                    "categoryName": "Deductible",
+                    "value": "5000",
+                    "dataType": "Currency",
+                    "name": "Annual Deductible",
+                    "description": "Key"
+                }, {
+                    "categoryName": "Deductible",
+                    "value": "medical deductible",
+                    "dataType": "Text",
+                    "name": "Prescription Drug Deductible",
+                    "description": "Key"
+                }, {
+                    "categoryName": "Deductible",
+                    "value": "medical deductible",
+                    "dataType": "Text",
+                    "name": "Another Type of Deductible",
+                    "description": "Key"
+                }, {
+                    "categoryName": "Preventive Care",
+                    "value": "30",
+                    "dataType": "Currency",
+                    "name": "Primary Care Office Visit",
+                    "description": "Key"
+                }, {
+                    "categoryName": "Preventive Care",
+                    "value": "40",
+                    "dataType": "Currency",
+                    "name": "Speciality Office Visit",
+                    "description": "Key"
+                }, {
+                    "categoryName": "Preventive Care",
+                    "value": "45",
+                    "dataType": "Currency",
+                    "name": "Non-Physican Office Visit"
+                }],
+                "NumDays": "1",
+                "ProductName": "Tiered Copayment HMO 250",
+                "ProductRecommend": true,
+                "ProductCode": "TI-EM",
+                "NumChildren": "2",
+                "NumAdults": "2",
+                "TotalPrice": "12.9",
+                "ChildPrice": "1.2",
+                "AdultPrice": "2.75",
+                "ID": "1"
             }, {
-                "categoryName": "Preventive Care",
-                "value": "30",
-                "dataType": "Percent",
-                "name": "Primary Care Office Visit"
+                "attributes": [{
+                    "categoryName": "Deductible",
+                    "value": "6000",
+                    "dataType": "Currency",
+                    "name": "Annual Deductible",
+                    "description": "Key"
+                }, {
+                    "categoryName": "Deductible",
+                    "value": "medical deductible",
+                    "dataType": "Text",
+                    "name": "Prescription Drug Deductible",
+                    "description": "Key"
+                }, {
+                    "categoryName": "Preventive Care",
+                    "value": "30",
+                    "dataType": "Percent",
+                    "name": "Primary Care Office Visit",
+                    "description": "Key"
+                }, {
+                    "categoryName": "Preventive Care",
+                    "value": "40",
+                    "dataType": "Percent",
+                    "name": "Speciality Office Visit",
+                    "description": "Key"
+                }, {
+                    "categoryName": "Preventive Care",
+                    "value": "45",
+                    "dataType": "Percent",
+                    "name": "Non-Physican Office Visit"
+                }],
+                "NumDays": "1",
+                "ProductName": "Tiered Copayment HMO 500",
+                "ProductRecommend": false,
+                "ProductCode": "TI-NE",
+                "NumChildren": "2",
+                "NumAdults": "2",
+                "TotalPrice": "10.1",
+                "ChildPrice": "0.8",
+                "AdultPrice": "1.75",
+                "ID": "2"
             }, {
-                "categoryName": "Preventive Care",
-                "value": "40",
-                "dataType": "Currency",
-                "name": "Speciality Office Visit"
-            }, {
-                "categoryName": "Preventive Care",
-                "value": "45",
-                "dataType": "Currency",
-                "name": "Non-Physican Office Visit"
-            }, {
-                "categoryName": "Preventive Care",
-                "value": "45",
-                "dataType": "Currency",
-                "name": "Another Name"
+                "attributes": [{
+                    "categoryName": "Deductible",
+                    "dataType": "Percent",
+                    "name": "In Network",
+                    "value": "50"
+                }, {
+                    "categoryName": "Deductible",
+                    "dataType": "Percent",
+                    "name": "In Network",
+                    "value": "50"
+                }, {
+                    "categoryName": "Deductible",
+                    "dataType": "Currency",
+                    "name": "In Network Individual",
+                    "value": "60"
+                }, {
+                    "categoryName": "Deductible",
+                    "dataType": "Currency",
+                    "name": "Out of Network",
+                    "value": "30"
+                }, {
+                    "categoryName": "Features",
+                    "dataType": "Currency",
+                    "name": "Baggage Loss, Delay & Damage",
+                    "value": "40"
+                }, {
+                    "categoryName": "Features",
+                    "dataType": "Text",
+                    "name": "Emergency Medical",
+                    "value": "Included",
+                    "description": "Monkey"
+                }, {
+                    "categoryName": "Features",
+                    "dataType": "Text",
+                    "name": "Flight & Travel Accident",
+                    "value": "Included"
+                }, {
+                    "categoryName": "Features",
+                    "dataType": "Text",
+                    "name": "Trip Cancellation & Interruption",
+                    "value": "Not Included"
+                }],
+                "AdultPrice": 1.75,
+                "ChildPrice": 0.8,
+                "ID": "0",
+                "NumAdults": 2,
+                "NumChildren": 2,
+                "NumDays": 1,
+                "ProductCode": "TI-NE",
+                "ProductName": "Non-Emergency Medical",
+                "TotalPrice": 10.1
             }],
-            "NumDays": "1",
-            "ProductName": "HMO 200",
-            "ProductRecommend": "true",
-            "ProductCode": "TI-AI",
-            "NumChildren": "2",
-            "NumAdults": "2",
-            "TotalPrice": "26.8",
-            "ChildPrice": "3.4",
-            "AdultPrice": "7.5",
-            "ID": "0"
-        }, {
-            "attributes": [{
-                "categoryName": "Deductible",
-                "value": "5000",
-                "dataType": "Currency",
-                "name": "Annual Deductible"
-            }, {
-                "categoryName": "Deductible",
-                "value": "medical deductible",
-                "dataType": "Text",
-                "name": "Prescription Drug Deductible"
-            }, {
-                "categoryName": "Deductible",
-                "value": "medical deductible",
-                "dataType": "Text",
-                "name": "Another Type of Deductible"
-            }, {
-                "categoryName": "Preventive Care",
-                "value": "30",
-                "dataType": "Currency",
-                "name": "Primary Care Office Visit"
-            }, {
-                "categoryName": "Preventive Care",
-                "value": "40",
-                "dataType": "Currency",
-                "name": "Speciality Office Visit"
-            }, {
-                "categoryName": "Preventive Care",
-                "value": "45",
-                "dataType": "Currency",
-                "name": "Non-Physican Office Visit"
-            }],
-            "NumDays": "1",
-            "ProductName": "HMO 250",
-            "ProductRecommend": "false",
-            "ProductCode": "TI-EM",
-            "NumChildren": "2",
-            "NumAdults": "2",
-            "TotalPrice": "12.9",
-            "ChildPrice": "1.2",
-            "AdultPrice": "2.75",
-            "ID": "1"
-        }, {
-            "attributes": [{
-                "categoryName": "Deductible",
-                "value": "6000",
-                "dataType": "Currency",
-                "name": "Annual Deductible"
-            }, {
-                "categoryName": "Deductible",
-                "value": "medical deductible",
-                "dataType": "Text",
-                "name": "Prescription Drug Deductible"
-            }, {
-                "categoryName": "Preventive Care",
-                "value": "30",
-                "dataType": "Percent",
-                "name": "Primary Care Office Visit"
-            }, {
-                "categoryName": "Preventive Care",
-                "value": "40",
-                "dataType": "Percent",
-                "name": "Speciality Office Visit"
-            }, {
-                "categoryName": "Preventive Care",
-                "value": "45",
-                "dataType": "Percent",
-                "name": "Non-Physican Office Visit"
-            }],
-            "NumDays": "1",
-            "ProductName": "HMO 500",
-            "ProductRecommend": "false",
-            "ProductCode": "TI-NE",
-            "NumChildren": "2",
-            "NumAdults": "2",
-            "TotalPrice": "10.1",
-            "ChildPrice": "0.8",
-            "AdultPrice": "1.75",
-            "ID": "2"
-        }],
-        "error": "OK",
-        "currencyCode": "USD"
+            "error": "OK",
+            "currencyCode": "GBP"
+        }
     };
-
-    $scope.open = function(size) {
-
-        var modalInstance = $modal.open({
-            templateUrl: 'vlcCompareModal.html',
-            controller: 'CompareModalCtrl',
-            size: size,
-            resolve: {
-                content: function() {
-                    return $scope.productsJSON;
-                }
-            }
-        });
-
-        modalInstance.result.then(function(selectedItem) {
-            $scope.selected = selectedItem;            
-        }, function() {
-            $log.info('Modal dismissed at: ' + new Date());
-        });
-    };
-});
-
-bpModule.controller('CompareModalCtrl', function($scope, $modalInstance, content) {
-    $scope.categoryList = [];
-    $scope.categoryMap = {};
-    $scope.productList = [];
     $scope.currencySymbol = "";
-    $scope.currencyCode = content.currencyCode;
+    $scope.currencyCode = $scope.control.vlcSI.currencyCode;
     $scope.currencyMap = [
         {"USD": "$"},
         {"CAD": "$"},
@@ -179,6 +222,40 @@ bpModule.controller('CompareModalCtrl', function($scope, $modalInstance, content
         return currencySymbol;
     };
     $scope.currencySymbol = $scope.getCurrencySymbol($scope.currencyCode);
+
+    $scope.open = function(size) {
+
+        var modalInstance = $modal.open({
+            templateUrl: 'vlcCompareModal.html',
+            controller: 'CompareModalCtrl',
+            size: size,
+            resolve: {
+                content: function() {
+                    return $scope.control.vlcSI;
+                },
+                currencySymbol: function() {
+                    return $scope.currencySymbol;
+                },
+                currencyCode: function() {
+                    return $scope.currencyCode;
+                }
+            }
+        });
+
+        modalInstance.result.then(function(selectedItem) {
+            $scope.selected = selectedItem;            
+        }, function() {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+});
+
+bpModule.controller('CompareModalCtrl', function($scope, $modalInstance, content, currencySymbol, currencyCode) {
+    $scope.categoryList = [];
+    $scope.categoryMap = {};
+    $scope.productList = []; 
+    $scope.currencySymbol = currencySymbol;
+    $scope.currencyCode = currencyCode;
 
     $scope.attributesValueForProduct = function(productCode, categoryCode, attributeName) {
         return $scope.categoryMap[categoryCode]['attributes'][attributeName]['assignmentValues'][productCode];
